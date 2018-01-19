e2c30afdd840827c77947f2eb691cc5476bdad34
Dmytro Soroka <diem9@yandex.ru>
2018-01-19 18:32:00 +0200

    users auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/', function () {
+//   return view('welcome');
+// });
+
+// Route::get('/', ['as' => 'frontpage', 'uses' => 'ReportController@index']);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
